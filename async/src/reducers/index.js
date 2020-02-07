c43b6abacc90021ed6324ea980a935fc7324d7a8
Raudel Flores <rawdell@outlook.com>
2020-02-06 21:31:06 -0600

    set up the reducer, cirrently no cases but created the initial state to be returned

---
+++ async/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+export const initialState = {
+    isFetching: false,
+    catImg: '',
+    error: ''
+}
+
+export const catImgReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
