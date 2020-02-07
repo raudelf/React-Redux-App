fb1cac69acfa0fff2735625a9889651f5bfdf8c5
Raudel Flores <rawdell@outlook.com>
2020-02-06 21:59:52 -0600

    set up the case returns for the reducer when the Fetch_cats and Update_img are called

--- async/src/reducers/index.js
+++ async/src/reducers/index.js
@@ -1,3 +1,4 @@
+import { FETCH_CATS, UPDATE_IMG } from '../actions';
 
 export const initialState = {
     isFetching: false,
@@ -7,6 +8,18 @@ export const initialState = {
 
 export const catImgReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_CATS:
+            return {
+                ...state,
+                isFetching: true,
+                catImg:''
+            }
+        case UPDATE_IMG:
+            return {
+                ...state,
+                isFetching: false,
+                catImg: action.payload
+            }
         default:
             return state;
     }
